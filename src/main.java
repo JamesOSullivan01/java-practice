f8616a09e3b56903cb43c5e3c38ba15f1f26048d
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-17 17:49:21 -0500

    Completed 'Java programming masterclass' lesson on code blocks and if else statements.

--- src/main.java
+++ src/main.java
@@ -19,9 +19,21 @@ public class main {
         myVariable--;
 
         System.out.println("This is a test");
-        if (myVariable == 0){
+        if (myVariable == 0) {
             System.out.println("It's now zero");
         }
-    }
 
+        boolean gameOVer = true;
+        int score = 400;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score <= 5000 && score > 1000) {
+            System.out.println("Your score was less than  5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("got here");
+        }
+    }
 }

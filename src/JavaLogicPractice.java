2924abe5011f3144425561f2e8c4710b0a141e81
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-14 22:46:13 -0500

    completed basic java problem.

--- src/JavaLogicPractice.java
+++ src/JavaLogicPractice.java
@@ -553,16 +553,14 @@ public class JavaLogicPractice {
         return welcomeString.toString();
     }
 
-//    **Way better solution
-//  public String sayHello(String [] name, String city, String state){
-//    StringBuilder welcomeString = new StringBuilder();
-//    String newString = Arrays.toString(name);
-//        String str = newString.replace("[", "").replace("]", "").replace(",", "");
-//    welcomeString.append("Hello, " + str + "!" + " Welcome to " + city + ", " + state + "!");
-//    return welcomeString.toString();
-//  }
 
-
+// Nathan loves cycling.
+//Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+//You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+    public int Liters(double time)  {
+        double liters = time / 2.0;
+        return (int) Math.floor(liters);
+    }
 
 
 }
